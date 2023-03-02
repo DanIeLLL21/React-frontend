28efd3979591bb5cb6315d52507e827eb3b5f714
unknown <daniel.persic32@gmail.com>
2023-03-02 06:13:57 -0800

    last commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import ReactPaginate from 'react-paginate'
 
 function App() {
 
-
+  const URL = "https://dummyjson.com/products?limit=10";
   // Variables to store data,API url, and each product that we want to display.
 
-  const URL = "https://dummyjson.com/products?limit=10";
   let [productData,setProductData] = useState([]);
-  let [showProduct,setShowProduct] = useState(false);
+  let [showProductList,setShowProductList] = useState(false);
   let [eachProduct,setEachProduct] = useState({
     title:"",
     description:"",
@@ -24,7 +23,7 @@ function App() {
   })
 
   const [page, setPage] = useState(0),
-  maxPage = Math.ceil(productData.length/2)
+  maxPage = Math.ceil(productData.length/1)
 
 
    // Functions to make application work. For getting data,handling clicks and showing component.
@@ -33,13 +32,12 @@ function App() {
       const res = await fetch(URL);
       const data = await res.json();
 
-
       setProductData(data.products);
-      console.log(data)
     }
 
   const handlePageClick = (click) => {
-    setPage(click.selected)
+    let pageNumber = click.selected;
+    setPage(pageNumber);
   }
 
   const showEach = (product) => {
@@ -55,7 +53,7 @@ function App() {
         imagesURL:[product.images]
       })
 
-      setShowProduct(true);
+      setShowProductList(true);
   }
 
 
@@ -68,7 +66,7 @@ function App() {
 
   // Condition to render Product component if user clicks on image or heading.
 
-  if(showProduct) {
+  if(showProductList) {
     return <ProductComponent
 
     name={eachProduct.title} 
@@ -88,17 +86,19 @@ function App() {
 
     return (
    <div className="product-container">
+
         {
           productData
             .slice(page*1,1*(page+1))
             .map((content,key) => (
               <div>
                  <h1 className="product-title-heading" onClick={()=>showEach(content)}>{content.title}</h1>
-                  <p>{content.description}</p>
+                  <p className="product-description">{content.description}</p>
                   <img className="product-image" onClick={()=>showEach(content)} src={content.thumbnail.toString()}/>
               </div>
            ))
         }
+        <div className="pagination-container"> 
         <ReactPaginate
         previousLabel={'previous'}
         nextLabel={'next'}
@@ -111,6 +111,7 @@ function App() {
         previousLinkClassName={'page-link'}
         pageLinkClassName={'page-link'}
         />
+        </div>
     </div>
   );
 
--- src/components.js
+++ src/components.js
@@ -5,35 +5,7 @@ import './App.css'
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 
-const spanStyle = {
-  padding: '20px',
-  background: '#efefef',
-  color: '#000000'
-}
 
-const divStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  backgroundSize: 'cover',
-  height: '400px',
-  width:'400px'
-}
-
-const productImageCont = {
-  width:"400px"
-}
-
-
-const productDetailsContainer = {
-  backgroundColor:"yellow"
-}
-
-const productContainer = {
-  display:"flex",
-  justifyContent:"space-evenly",
-  marginTop:"30vh"
-}
 
 function ProductComponent (props) {
 
@@ -43,41 +15,42 @@ function ProductComponent (props) {
 		return <App/>
 	}
 
-	let styles = {
-		imageStyle:{
-			width:'200px'
-		},
-    buttonStyle:{
-      borderRadius:"13px",
-      width:"100px",
-      cursor:"pointer"
-    }
-	}
-
-
 	return (
 		<div className="eachproduct-container">
-    <div style={productDetailsContainer}>
-		  <h1>{props.name} </h1>
-		  <p>Descp :{props.description}</p>
-      <p>Brand :{props.brand}</p>
-      <p>Cat : {props.category}</p>
-      <p>Price :{props.price}</p>
-      <p>Rating: {props.rating}</p>
-      <p>Stock :{props.stock}</p>
-      <button style={styles.buttonStyle} onClick={()=>setGoBack(true)}>Go back</button>
-    </div>
-    <div style={productImageCont}>
+    <div className="eachproduct-image-container">
     <Slide>
-		{props.image[0].map((a,i)=>{
+    {props.image[0].map((a,i)=>{
             return(
              <div key={i}>
-              <div styles={styles.imageStyle} style={{ ...divStyle, 'backgroundImage': `url(${a})` }}>
+              <div  className="eachproduct-image-slider" style={{'backgroundImage': `url(${a})` }}>
               </div>
             </div>)
     })}
       </Slide>
      </div>
+    <div className="eachproduct-details-container">
+		  <h1 className="eachproduct-details-heading">{props.name}</h1>
+		  <p className="eachproduct-details-description">{props.description}</p>
+      <div className="eachproduct-details-brand-container">
+      <span class="eachproduct-brand-tag">
+        <a>BRAND</a>
+      </span>
+      <span class="eachproduct-brand-name">
+        <a>{props.brand}</a>
+      </span>
+      </div>
+      <div className="eachproduct-details-pricing">
+      <ul className="eachproduct-details-pricinglist">
+        <li className="eachProduct-details-list price"><a>PRICE</a> <span>{props.price}$</span></li>
+        <li className="eachProduct-details-list ">RATING <span>{props.rating}</span></li>
+        <li className="eachProduct-details-list "><span>STOCK</span> <span>{props.stock}</span></li>
+      </ul>
+      </div>
+      <div className="eachproduct-details-buttons">
+      <button className="eachproduct-details-activebutton" onClick={()=>setGoBack(true)}>Go back</button>
+      <button className="eachproduct-details-disablebutton">Add to cart</button>
+      </div>
+    </div>
 		</div>
 		)
 } 
